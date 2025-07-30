50a9a8610dda781fc6f942eb16f23d5837f5c90e
Mseif <seif4847@gmail.com>
2025-07-30 16:20:18 +0330

    Fix hydration mismatch by adding rtl-enabled class to html element

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="fa" dir="rtl">
+    <html lang="fa" dir="rtl" className="rtl-enabled">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
